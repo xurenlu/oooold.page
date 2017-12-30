09038cacd12b01d0cf603c33dea81e97ef953708
黄滚 <renlu.xu@taobao.com>
2017-12-31 01:33:58 +0800

    更新了oss的镜像回源

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'app.js'
+    filename: 'app2.js'
   },
   module: {
     rules: [
